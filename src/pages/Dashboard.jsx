da94e3c349786782e2025eb8d0351699bb531eb1
vickhyathBhora <vickycoder0100100101000001@gmail.com>
2026-08-28 15:54:37 +0530

    testing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,7 +17,7 @@ function DashboardContent() {
       return null;
     }
     try {
-      const response = await fetch('http://localhost:5000/api/contacts/offline_unseen', {
+      const response = await fetch('https://cliveback-end-production.up.railway.app/api/contacts/offline_unseen', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
